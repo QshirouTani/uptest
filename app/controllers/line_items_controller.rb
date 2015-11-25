fa1317e8bd5c4d61e5c6edca6d5ad2d86eecf3c8
Qtani <xx@xxxx>
2015-11-25 04:50:49 +0000

    Add JQuery Code

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -35,6 +35,7 @@ class LineItemsController < ApplicationController
         #format.json { render :show, status: :created, location: @line_item }
         format.html { redirect_to @line_item.cart}
         #format.json { render json: @line_item, status: :created, location: @line_item } #カート削除機能追加に付き機能停止
+        format.json { @current_item = @line_item}
         format.json { render json: @line_item}
       else
         format.html { render action: "new" }
@@ -48,7 +49,7 @@ class LineItemsController < ApplicationController
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
+        format.html { redirect_to store_url, notice: 'Line item was successfully updated.' }
         format.json { render :show, status: :ok, location: @line_item }
       else
         format.html { render :edit }
